83260b91562424caea906207b7db4dbae5bdff0e
CoderRahul9904 <mouryarahul543@gmail.com>
2024-10-29 21:50:35 +0530

    working

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./main.css"
 
 const App = () => {
   // const [selectedText, setSelectedText] = useState('');
@@ -25,7 +26,7 @@ const App = () => {
   //   chrome.runtime.sendMessage({ action: "callGeminiAPI", text: selectedText }, (response) => {
   //     if (response && response.result) {
   //       setApiResponse(response.result);
-        
+
   //       // Now replace the selected text in the DOM
   //       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   //         const tabId = tabs[0]?.id;
@@ -36,23 +37,16 @@ const App = () => {
   //     }
   //   });
   // };
-  
+
 
   console.log("From app")
 
   return (
     <div>
-      <h1>ClipClarity</h1>
-      {/* <button onClick={fetchSelectedText}>Get Selected Text</button>
-      <p>Selected Text: {selectedText}</p>
-
-      {selectedText && (
-        <button onClick={callGeminiAPI}>Get Explanation</button>
-      )}
-
-      {apiResponse && (
-        <p>API Response: {apiResponse}</p>
-      )} */}
+      <div className="text-white flex flex-col justify-center items-center text-center gap-1">
+        <h3 className="m-0 font-bold">Welcome to Clip Clarity!</h3>
+        <p className="m-0 text-sm">Highlight text on any webpage and use the extension to simplify or get desired insights instantly.</p>
+      </div>
     </div>
   );
 };
