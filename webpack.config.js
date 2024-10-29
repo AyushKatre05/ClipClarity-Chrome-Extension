f6e3547c4f7d2fb768e9b524d79a4e29f93ef3a6
CoderRahul9904 <mouryarahul543@gmail.com>
2024-10-29 21:52:13 +0530

    working

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
+
+const autoprefixer = require('autoprefixer');
+const tailwindcss=require('tailwindcss')
 const path = require('path');
+
 
 module.exports = {
     mode: "development",
     entry: {
-        html: "./src/index.tsx",
         background: "./public/background.js",
         contentScript: "./public/contentScript.js",
-        popup: "./public/popup.js"
+        popup: "./public/popup.js",
+        index:"./src/index.tsx"
     },
     
     output: {
@@ -23,6 +27,18 @@ module.exports = {
                 test: /\.tsx?$/, // Test for both .ts and .tsx files
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                use:['style-loader','css-loader',{
+                    loader: 'postcss-loader',
+                    options:{
+                        postcssOptions:{
+                            indent: 'postcss',
+                            plugins:[tailwindcss,autoprefixer]
+                        }
+                    }
+                }],
+                test:/\.css$/i
             }
         ]
     }
